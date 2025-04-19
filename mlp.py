3bca90071b5f3ac7a7002ecec1736ca3bfdb3897
Stephanie Hayden <shayden8@student.gsu.edu>
2025-04-19 17:41:37 -0400

    Complete forword propogation with loss reported

--- mlp.py
+++ mlp.py
@@ -4,8 +4,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import torch
 
-sigmoid=lambda x: 1 / (1 + np.e^(-x))
-#manually define softmax function
+np.set_printoptions(precision=4, suppress=False)
 
 def dataloader(train_dataset, test_dataset, batch_size=128):
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
@@ -19,19 +18,32 @@ def load_data():
     print("The number of training data:", len(train_dataset))
     print("The number of testing data:", len(test_dataset))
     return dataloader(train_dataset, test_dataset)
+#Numerically stable softmax, axis=0 means that the computation happens column by column
+def softmax(matrix_array):
+    matrix_array_stable = matrix_array - np.max(matrix_array, axis=0, keepdims=True)
+    matrix_array_stable= np.exp(matrix_array_stable)
+    matrix_array_stable = matrix_array_stable / np.sum(matrix_array_stable, axis=0, keepdims=True)
+    return matrix_array_stable
+#numerically stable sigmoid, when there are large negative values must compute e^x/(1+e^x)
+def sigmoid(matrix_array):
+    return (np.where(matrix_array<0, 
+    np.exp(matrix_array)/ (1 + np.exp(matrix_array)),
+    1 / (1+np.exp(-matrix_array))
+    ))
 
 class MLP:
-    def __init__(self, input_size, hidden_size, output_size,lr):  # building the model
+    def __init__(self, input_size, hidden_size, output_size, lr):  # building the model
         self.input_size=input_size
         self.hidden_size=hidden_size
         self.output_size=output_size
         self.lr=lr
         #initializing weights and bias terms, will update in backward()
-        self.weights_hl1=torch.rand(input_size, hidden_size)
-        self.bias_hl1=torch.zeros(hidden_size)
+        rng=np.random.default_rng()
+        self.w_1=rng.normal(0, 1, (hidden_size, input_size))
+        self.b_1=np.zeros((hidden_size,1), dtype=np.float64)
 
-        self.weights_hl2=torch.rand(hidden_size, output_size)
-        self.bias_hl2=torch.zeros(output_size)
+        self.w_2=rng.normal(0, 1, (output_size, hidden_size))
+        self.b_2=np.zeros((output_size,1), dtype=np.float64)
 
         """Building hidden layers:
 
@@ -46,51 +58,55 @@ class MLP:
         """
     
     def forward(self, x):  # forward propagation to get predictions
-        #assuming x is a tensor with dimensions (128,724)
-        weighted_input_hl1=torch.matmul(x, self.weights_hl1)
-        output_hl1=weighted_input_hl1 + self.bias_hl1
-        activated_output_hl1=output_hl1.apply(sigmoid)
-        #activated_output should have dimensions [batch_size, hidden_size]
+        #assuming x is a matrix with dimensions (input_size, batch_size)
+        batch_size=x.shape[1]
+        z_1=np.matmul(self.w_1, x) + np.matmul(self.b_1, np.ones((1, batch_size)))
+        #apply sigmoid function elementwise to z_1 
+        a_1 = sigmoid(z_1)
+        #a_1 should have dimensions [hidden_size, batch_size]
 
-        weighted_input_hl2=torch.matmul(activated_output_hl1, self.weights_hl2)
-        output_hl2=weighted_input_hl2 + self.bias_hl2
-        #computing softmax along the rows of hl2, will implement manually later
-        outputs=torch.nn.functional.softmax(output_hl2, dim=1)
-        return outputs
+        z_2=np.matmul(self.w_2, a_1) + np.matmul(self.b_2, np.ones((1, batch_size)))
+        #z_2 has dimensions output_size, batch_size, apply softmax function on columns
+        #call the vectorized softmax operation on z_2, a_2 has dimensions [output_size, batch_size]
+        a_2=softmax(z_2)
+        return a_2
     
     def backward(self, x, y, pred):
         print(f'Dimensions of pred should be (128,10) {pred.size()}') 
         print(f'Dimensions of y should be (128) atp {y.size()}')
         print(f'Dimensions of x should be (128, 724) {x.size()}')
         # one-hot encode the labels
+        """ 
         encoded_y=torch.zeros(128,10)
         for i in range(y.size(dim=0)):
             encoded_y(i, y(i))=1
+            """
         # compute the gradients
         
         # update the weights and biases
         
 
     def train(self, x,y):
-        print('in progress')
         loss=0
-        # call forward function
-        predictions=self.forward(x)
-        # calculate loss
-        encoded_y=torch.zeros_like(y)
-        for i in range(y.size(dim=0)):
-            encoded_y(i, y(i))=1
-        #encoded_y and y are both tensors with dimensions (128,10)
-        for i in range(y.size(dim=0)):
-            loss += y[i,:]*torch.log(encoded_y[i,:]) + (1-y[i,:])*torch.log(1-encoded_y[i,:])
-        loss /= (-1/y.size(dim=0))
-        # call backward function
-
+        batch_size=x.shape[1]
+        
+        y_hat=self.forward(x)
+        #One hot encode the vectors for y, then calculate the loss with cross entropy
+        #Call backwards update weights and biases within backwards return the loss and then train is called again in the loop
+        
+        encoded_y=np.zeros((self.output_size, batch_size))
+        for i in range(y.shape[0]):
+            encoded_y[y[i], i]=1
+        #encoded_y and y are both tensors with dimensions (10, 128)
+        #cross entropy loss by looping through columns might be able to vectorize
+        for i in range(batch_size):
+            loss += np.sum(-encoded_y[:,i] * np.log(y_hat[:,i]))
+        print(loss)
         return loss
 
 def main():
     #Define instance of MLP globally so that it's training data (weights and biases) persists when using it on testing data
-    
+    #Could make input size a tuple with batch size included, could make batch size a global variable? 
     # First, load data
     train_loader, test_loader = load_data()
     # Second, define hyperparameters
@@ -100,16 +116,18 @@ def main():
     learning_rate=.2
     model=MLP(input_size, hidden_size, output_size, learning_rate)
     num_epochs = 100
-    batch_size=128
     # Then, train the model
     for epoch in range(num_epochs):
         total_loss = 0
 
-        for inputs, labels in train_loader:  # define training phase for training model
-            print(f'Dimensions of inputs: {inputs.size()}, dimensions of lables: {labels.size()}')
-            print(labels)
-
-            total_loss += model.train(inputs, labels)
+        for inputs, labels in train_loader:  # Loop is for mini batch training 
+            x=np.empty((784,128))
+            inputs=inputs.numpy()
+            for i in range(inputs.shape[0]):
+                x[:, i]=np.ravel(inputs[i,...]) 
+            labels=labels.numpy()
+            
+            total_loss += model.train(x, labels)
         print(f"Epoch {epoch+1}/{num_epochs}, Loss: {total_loss/len(train_loader)}") # print the loss for each epoch
 
     # Finally, evaluate the model
